3fb2670b7e9a1eb0563ecdcfb5ac98d0429e3796
npapoutsakis <nikospapoutsak@gmail.com>
2022-12-15 19:34:35 +0200

    packet_handler done, extract upd/tcp info

--- pcap_ex.c
+++ pcap_ex.c
@@ -5,6 +5,8 @@
 
 #include <netinet/ether.h>
 #include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
 
 
 // Declaring global counters
@@ -21,41 +23,89 @@ int total_bytes_tcp = 0;
 int total_bytes_udp = 0;
 
 
+enum protocol{
+	UDP,
+	TCP
+};
 
-typedef struct networkFlow {
+struct networkFlow {
+	char *source_ip;
+	char *destination_ip;
+
+	int source_port;
+	int destination_port;
+
+	enum protocol type;
+};
 
 
 
-		
 
-} networkFlow;
+/// @brief Print the statistics, aka global counters 
+void statistics(void){
+	printf("+----------------------------------------------+\n");
+	printf("|                  Statistics                  |\n");
+	printf("+----------------------------------------------+\n");
+	printf("  [+] Total network flows captured: %d\n", total_network_flows);
+	printf("  [+] Total TCP network flows captured: %d\n", tcp_network_flows);
+	printf("  [+] Total UDP network flows captured: %d\n", udp_network_flows);
+	printf("  [+] Total packets captured: %d\n", total_packets);
+	printf("  [+] Total TCP packets captured: %d\n", total_tcp_packets);
+	printf("  [+] Total UDP packets captured: %d\n", total_udp_packets);
+	printf("  [+] Total bytes of TCP packets captured: %d\n", total_bytes_tcp);
+	printf("  [+] Total bytes of UDP packets captured: %d\n", total_bytes_udp);
+	return;
+}
+
+
+
+void extractTCP(const u_char *pkt_data, int size){
+
+
+
+	return;
+}
+
+
+void extractUDP(const u_char *pkt_data, int size){
+
+
+
+	return;
+}
 
 
 /// @brief Callback function invoked by libpcap for every incoming packet
 void packet_handler(u_char *user, const struct pcap_pkthdr *pkt_header, const u_char *pkt_data){
+
+	// Increase total number of packets
+	total_packets++;
 	
 	// Here will see what the packet contains
 	// Useful information: https://www.winpcap.org/docs/docs_40_2/html/group__wpcap__tut6.html
-	struct iphdr *ih = NULL;
+	struct iphdr *ip_header = (struct iphdr *)(pkt_data + sizeof(struct ethhdr));
 	
-	ih = (struct iphdr *)(pkt_data + sizeof(struct ethhdr));
+	if(ip_header == NULL)	
+		exit(EXIT_FAILURE);
 
-	switch(ih->protocol){
+	// struct ethhdr *eth_header = (struct ethhdr *)(pkt_data);
+	// printf("%d\n", ip_header->protocol);
+
+	switch(ip_header->protocol){
 		//17
 		case IPPROTO_UDP:
 			total_udp_packets++;
+			extractUDP(pkt_data, pkt_header->len);
 			break;
 		//6
 		case IPPROTO_TCP:
 			total_tcp_packets++;
+			extractTCP(pkt_data, pkt_header->len);
 			break;
-
 		default:
 			// Skipped!
 			break;
 	}
-
-	total_packets++;
 
 	return;
 }
@@ -83,10 +133,16 @@ void offline_monitor(char *filename){
 	
 	// Now that we have opened the file, read the packets and parse information
 	// A value of -1 or 0 for cnt is equivalent to infinity, so that packets are processed until another ending condition occurs.
-	pcap_loop(read_packets, 0, &packet_handler, NULL);
+	int returnVal = pcap_loop(read_packets, 0, &packet_handler, NULL);
+
+	if(returnVal == -1)
+		exit(-1);
 
 	// Close the opened file
 	pcap_close(read_packets);
+
+	// The statistics function can be placed here! pcap_loop will loop again and again until it reach EOF
+	statistics();
 
 	return;
 }
@@ -94,7 +150,6 @@ void offline_monitor(char *filename){
 
 /// @brief This function will start capturing traffic from a network interface
 void online_monitor(char *interface){
-
 
 	return;
 }
